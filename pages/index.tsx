983ffa5f754506223dff9a254458c8b29d6e3c5a
supat <supatachbeam@gmail.com>
2022-04-11 13:57:11 +0700

    refactor: delete unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from "next";
-import { useCallback, useContext, useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
-import { SocketContext } from "../contexts/socketUtils";
-import { Events } from "../utils/event";
 import PlayerCount from "../components/PlayerCount";
 
 const Home: NextPage = () => {

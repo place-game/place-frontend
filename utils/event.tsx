c056ae80adc1db0fce9f9c5a7d56d1bbc50ffb1d
supat <supatachbeam@gmail.com>
2022-04-11 15:34:46 +0700

    refactor: removed unused variables

--- utils/event.tsx
+++ utils/event.tsx
@@ -1,11 +1,8 @@
 export enum Events {
-    CREATE = 'create',
-    ON_CREATE = 'on-create',
     LEAVE = 'leave',
     ON_LEAVE = 'on-leave',
     JOIN = 'join',
     ON_JOIN = 'on-join',
-    INTRODUCE = 'introduce',
     GET_PLAYERS = 'get-players',
     ON_GET_PLAYERS = 'on-get-players',
   }
